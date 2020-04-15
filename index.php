90a7f2129f1b346fae9849cffce9b696923ee52c
irucairuca <62388961+irucairuca@users.noreply.github.com>
2020-04-15 21:47:07 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>電気の逃げ道</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+
+<header>
+    <nav>
+        <ul class="gnav-list">
+           <li><a href="https://irucairuca.github.io/denkinonigemichi/index.html">トップ</a></li>
+            <li><a href="https://irucairuca.github.io/denkinonigemichi/mokuji/index.html">目次</a></li>
+            <li><a href="https://irucairuca.github.io/denkinonigemichi/jikosixyoukai/index.html">イルカについて</a></li>
+        　　 <li><a href="https://irucairuca.github.io/denkinonigemichi/saitokousin/index.html">サイト更新</a></li>
+            <li><a href="https://irucairuca.github.io/denkinonigemichi/irucalink/index.html">イルカリンク</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+   <section>
+    <div class="haikei">   
+    <div class="text-box">   
+    <h1>電気の逃げ道へようこそ</h1>
+    <p>このサイトは管理人ことイルカがいろんなことから逃げまくってたらできてたサイトです</p>
+    <?php echo'こんにちは'?>
+    <li>大学の専門からは逃げ</li>
+    <li>語学からは逃げ</li>
+    <li>バイトからは逃げ...</li>
+    <p>こんな感じで逃げまくってきたのですが、別に逃げることは悪いことじゃない！気が向いたらまた戻っていくればいい！！そんな感じで、電気からも逃げたり戻ったりを繰り返していました。このサイトでは、電気を勉強していてつまずいたところをどのようにして乗り越えたかという勉強法やイルカの観点から見た電気工学の知見を発信していきたいと思います。</p>
+    <h2>↓連絡先<span>twitter</span></h2> 
+        <a href="https://twitter.com/arairuca"target="_blank"> <img class="twitter" src="twitter.jpg"></a>
+        
+        <p>何か質問等ございましたら<span>twitter</span>のDMでお願いします！！</p>    
+            </div>   
+       </div>
+    </section>
+
+    
+
+</main>
+
+</body>
+</html>
